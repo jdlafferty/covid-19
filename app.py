0087d4643e35b49e47ac9984a33ebf4693b20b92
John Lafferty <john.lafferty@yale.edu>
2020-04-02 12:46:43 -0500

    updated census address

--- app.py
+++ app.py
@@ -33,20 +33,28 @@ def county_state_of_address(addr):
 def process_most_recent_data():
     df_counties = pd.read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv", dtype={"fips": str})
     df_states = pd.read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv", dtype={"fips": str})
+    df_geo = pd.read_csv("https://raw.githubusercontent.com/jdlafferty/covid-19/master/geo-counties.csv", dtype={"fips": str})
+    df_census = pd.read_csv("https://raw.githubusercontent.com/jdlafferty/covid-19/master/data/county_2019_census.csv")
 
-    # determine most recent update
     last_date = max([dateutil.parser.parse(d) for d in np.array(df_counties['date'])])
     most_recent_date = last_date.strftime("%Y-%m-%d")
     most_recent_date_long = last_date.strftime("%A %B %d, %Y")
     print("Most recent data: %s" % most_recent_date_long)
 
-    # create data frames
     df_recent = df_counties[df_counties['date']==most_recent_date]
     df_recent = df_recent.sort_values('cases', ascending=False)
     df_recent = df_recent.reset_index().drop('index',1)
 
-    df_geo = pd.read_csv("https://raw.githubusercontent.com/jdlafferty/covid-19/master/geo-counties.csv", dtype={"fips": str})
     df_recent = pd.merge(df_recent, df_geo)
+    df_recent = pd.merge(df_recent, df_census, how='left', on=['county','state'])
+    df_recent = df_recent[df_recent['county'] != 'Unknown']
+    df_recent['population'] = np.array(df_recent['population'], dtype='int')
+
+    cases = np.array(df_recent['cases'])
+    population = np.array(df_recent['population'])
+    cases_per_100k = np.round(100000*np.array(cases/population),1)
+    df_recent['cases_per_100k'] = cases_per_100k
+
     return (df_recent, most_recent_date_long)
 
 df, most_recent_date = process_most_recent_data()
@@ -54,7 +62,8 @@ df.head()
 
 def prepare_data_layout(df, address=None, min_cases=1, scale=3.0):
     df['text'] = df['county'] + ', ' + df['state'] + '<br>' + \
-        (df['cases']).astype(str) + ' cases, ' + (df['deaths']).astype(str) + ' deaths'
+        (df['cases']).astype(str) + ' cases, ' + (df['deaths']).astype(str) + ' deaths<br>' + \
+        (df['cases_per_100k']).astype(str) + ' cases per 100k people'
     df = df[df['cases'] >= min_cases]
     df = df[df['county']!='Unknown']
     df['type'] = np.zeros(len(df))
