1ce8a2bca983f79e4300bc27ec96e1df3d983aec
John Lafferty <john.lafferty@yale.edu>
2020-04-07 23:19:21 -0500

    initial commit

---
+++ covid19.py
@@ -0,0 +1,143 @@
+
+import json
+import herepy
+import plotly
+import datetime
+import dateutil.parser
+import pandas as pd
+import numpy as np
+
+# initial geocoder and read in NYTimes data
+geocoderApi = herepy.GeocoderApi('VbY-MyI6ZT9U8h-Y5GP5W1YaOzQuvNnL4aSTulNEyEQ')
+
+def lat_lon_of_address(addr):
+    response = geocoderApi.free_form(addr)
+    result = response.as_json_string()
+    res = eval(result)
+    (lat, lon) = (res['Response']['View'][0]['Result'][0]['Location']['DisplayPosition']['Latitude'],
+                  res['Response']['View'][0]['Result'][0]['Location']['DisplayPosition']['Longitude'])
+    return (lat, lon)
+
+def county_state_of_address(addr):
+    response = geocoderApi.free_form(addr)
+    result = response.as_json_string()
+    res = eval(result)
+    state = res['Response']['View'][0]['Result'][0]['Location']['Address']['AdditionalData'][1]['value']
+    county = res['Response']['View'][0]['Result'][0]['Location']['Address']['AdditionalData'][2]['value']
+    return (county, state)
+
+def process_most_recent_data():
+    last_date = max([dateutil.parser.parse(d) for d in np.array(df_counties['date'])])
+    most_recent_date = last_date.strftime("%Y-%m-%d")
+    most_recent_date_long = last_date.strftime("%A %B %-d, %Y")
+    print("covid19: Most recent NY Times data: %s" % most_recent_date_long)
+
+    df_recent = df_counties[df_counties['date']==most_recent_date]
+    df_recent = df_recent.sort_values('cases', ascending=False)
+    df_recent = df_recent.reset_index().drop('index',1)
+
+    df_recent = pd.merge(df_recent, df_geo)
+    df_recent = pd.merge(df_recent, df_census, how='left', on=['county','state'])
+    df_recent = df_recent[df_recent['county'] != 'Unknown']
+    df_recent['population'] = np.array(df_recent['population'], dtype='int')
+
+    cases = np.array(df_recent['cases'])
+    population = np.array(df_recent['population'])
+    cases_per_100k = np.round(100000*np.array(cases/population),1)
+    df_recent['cases_per_100k'] = cases_per_100k
+
+    return (df_recent, most_recent_date_long)
+
+
+def get_location_of_address(addr, df):
+    try:
+        response = geocoderApi.free_form(addr)
+        result = response.as_json_string()
+        res = eval(result)
+        (lat, lon) = (res['Response']['View'][0]['Result'][0]['Location']['DisplayPosition']['Latitude'],
+            res['Response']['View'][0]['Result'][0]['Location']['DisplayPosition']['Longitude'])
+        state = res['Response']['View'][0]['Result'][0]['Location']['Address']['AdditionalData'][1]['value']
+        county = res['Response']['View'][0]['Result'][0]['Location']['Address']['AdditionalData'][2]['value']
+        if df[(df['county']==county) & (df['state']==state)].shape[0] == 0:
+            raise Exception('InvalidStateCounty')
+        return ((lat, lon), (county, state))
+    except:
+        raise Exception('InvalidAddress')
+
+
+def prepare_data_layout(df, address=None, mark='cases', min_cases=1, scale=3.0):
+    df['text'] = df['county'] + ', ' + df['state'] + '<br>' + \
+        (df['cases']).astype(str) + ' cases, ' + (df['deaths']).astype(str) + ' deaths<br>' + \
+        (df['cases_per_100k']).astype(str) + ' cases per 100k people'
+    df = df[df['cases'] >= min_cases]
+    df = df[df['county']!='Unknown']
+    df['type'] = np.zeros(len(df))
+
+    if address != None:
+        try:
+            ((this_lat, this_lon), (this_county, this_state)) = get_location_of_address(address, df)
+            county_record = df[(df['county']==this_county) & (df['state']==this_state)]
+            this_text = '%s<br>County: %s' % (address, np.array(county_record['text'])[0])
+            td = pd.DataFrame(county_record)
+            cases = np.array([10000])
+            population = np.array(county_record['population'])
+            td['cases'] = cases
+            cases_per_100k = np.round(100000*np.array(cases/population),1)
+            td['cases_per_100k'] = cases_per_100k
+            td['text'] = [this_text]
+            td['type'] = [1]
+            df = df.append(td)
+        except:
+            print("Invalid address: " + address)
+
+    colors = ['rgba(255,0,0,0.2)', 'rgba(0,255,0,0.2)']
+    if mark=='cases':
+        sizes = df['cases']/scale
+    else:
+        sizes = df['cases_per_100k']/scale
+
+    data = [dict(type = 'scattergeo',
+            locationmode = 'USA-states',
+            lon = df['lon'],
+            lat = df['lat'],
+            text = df['text'],
+            marker = dict(
+                size = sizes,
+                color = pd.Series([colors[int(t)] for t in df['type']]),
+                line = dict(width = 0.5, color = 'black'),
+                sizemode = 'area'
+            ))
+        ]
+
+    title_text = "Data from The New York Times<br>github.com/nytimes/covid-19-data<br>%s" % most_recent_date
+
+    layout = dict(
+        width = 1400,
+        height = 800,
+        margin={"r":0,"t":0,"l":0,"b":0},
+        showlegend = False,
+        title = dict(
+            text = title_text,
+            y = 0.05,
+            x = 0.85,
+            xanchor = 'left',
+            yanchor = 'bottom',
+            font=dict(
+                family="Times New Roman",
+                size=10,
+                color="#7f7f7f"
+            )
+        ),
+        geo = dict(
+            scope = 'usa',
+            showland = True,
+            landcolor = 'rgb(240, 240, 240)'
+        )
+    )
+    return (data, layout)
+
+df_counties = pd.read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv", dtype={"fips": str})
+df_states = pd.read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv", dtype={"fips": str})
+df_geo = pd.read_csv("https://raw.githubusercontent.com/jdlafferty/covid-19/master/geo-counties.csv", dtype={"fips": str})
+df_census = pd.read_csv("https://raw.githubusercontent.com/jdlafferty/covid-19/master/data/county_2019_census.csv")
+df, most_recent_date = process_most_recent_data()
---
+++ covid19_predict.py
@@ -0,0 +1,199 @@
+import pandas as pd
+import numpy as np
+import covid19 as cvd
+import datetime
+import dateutil.parser
+import matplotlib.pyplot as plt
+
+
+def process_recent_data(days_back=7):
+    date = [dateutil.parser.parse(d) for d in np.array(cvd.df_counties['date'])]
+    last_date = max(date)
+    most_recent_date_long = last_date.strftime("%A %B %-d, %Y")
+
+    days = np.array([(last_date - d).days for d in date])
+    df_recent = cvd.df_counties[days < days_back]
+    df_recent = df_recent.sort_values('cases', ascending=False)
+    df_recent = df_recent.reset_index().drop('index', 1)
+
+    df_recent = pd.merge(df_recent, cvd.df_geo)
+    df_recent = pd.merge(df_recent, cvd.df_census,
+                         how='left', on=['county', 'state'])
+    df_recent = df_recent[df_recent['county'] != 'Unknown']
+    df_recent['population'] = np.array(df_recent['population'], dtype='int')
+
+    cases = np.array(df_recent['cases'])
+    population = np.array(df_recent['population'])
+    cases_per_100k = np.round(100000*np.array(cases/population), 1)
+    df_recent['cases_per_100k'] = cases_per_100k
+
+    return (df_recent, most_recent_date_long)
+
+
+def compute_deltas(df, days_back=30):
+    nd = pd.DataFrame()
+    counties = list(df['county'])
+    states = list(df['state'])
+    state_county = set([sc for sc in zip(states, counties)])
+    for (state, county) in state_county:
+        this_county = pd.DataFrame(df[(df['county']==county) & (df['state']==state)])
+        cp100k = np.array(this_county['cases_per_100k'])
+        delta = np.append(np.maximum(-np.diff(cp100k), 0), np.nan)
+        this_county['delta'] = delta
+        nd = nd.append(this_county)
+
+    date = [dateutil.parser.parse(d) for d in np.array(nd['date'])]
+    last_date = max(date)
+    days = np.array([(last_date - d).days for d in date])
+    nd = nd[days < days_back]
+    nd = nd.sort_values(['cases', 'state', 'county'], ascending = (False, True, True))
+    return nd
+
+
+def initialize_for_simulation(df_delta):
+    print('covid19_predict: initializing for simulation')
+    df = df_delta.drop(['lat', 'lon', 'fips'], axis=1)
+    df_nonzero = df[df['delta'] > 0]
+    df = pd.DataFrame(df_nonzero)
+    df['z'] = np.log(np.array(df['delta'])/1e5) - np.log(1-np.array(df['delta'])/1e5)
+    df['psi'] = df['z']
+    df['var'] = 1/(np.array(df['delta'])*(1-np.array(df['delta']/1e5)))
+    df['d'] = 1/(1+1/np.array(df['var']))
+    df = df.reset_index().drop('index', 1)
+    return df
+
+
+def run_gibbs_sampler(df, B=10000, burn=1000):
+    k = df.shape[0]
+    v = np.sqrt(1/k)
+    n = df.shape[0]
+    print('covid19_predict: running Gibbs sampler: n=%d, B=%d' % (n, B))
+    psi = np.matrix(np.zeros(n*B)).reshape((B, n))
+    psi[0, :] = df['psi']
+    scale = np.sqrt(df['d'])
+    for b in np.arange(B-1):
+        loc = np.mean(psi[b, :])
+        mu = np.random.normal(loc=loc, scale=v)
+        e = df['d']*(mu + df['z']/df['var'])
+        psi[b+1, :] = np.random.normal(loc=e, scale=scale)
+    return psi[np.arange(burn, B), :]
+
+
+def make_predictions(df, psi):
+    # compute posterior and mean and credible interval
+    print('covid19_predict: making predictions')
+    psi_95 = np.percentile(psi, 95, axis=0)
+    psi_05 = np.percentile(psi, 5, axis=0)
+    psi_bar = np.array(np.mean(psi, axis=0))[0]
+    delta_bar = 1e5*np.exp(psi_bar)/(1+np.exp(psi_bar))
+    delta_95 = 1e5*np.exp(psi_95)/(1+np.exp(psi_95))
+    delta_05 = 1e5*np.exp(psi_05)/(1+np.exp(psi_05))
+    df['delta_bar'] = np.round(delta_bar, 2)
+    df['delta_95'] = np.round(delta_95, 2)
+    df['delta_05'] = np.round(delta_05, 2)
+    df = df.drop(['z', 'psi', 'd', 'var'], axis=1)
+
+    date = [dateutil.parser.parse(d) for d in np.array(df['date'])]
+    most_recent_date = max(date).strftime("%Y-%m-%d")
+    df = df[df['date'] == most_recent_date]
+
+    # make Bayes predictions for the most recent date
+    cases_predicted = (df['population']/100000)*(df['cases_per_100k']+df['delta_bar'])
+    cases_predicted = cases_predicted.astype(int)
+    df['cases_predicted'] = cases_predicted
+    cases_95_credible = (df['population']/100000)*(df['cases_per_100k']+df['delta_95'])
+    cases_95_credible = cases_95_credible.astype(int)
+    cases_05_credible = (df['population']/100000)*(df['cases_per_100k']+df['delta_05'])
+    cases_05_credible = cases_05_credible.astype(int)
+    df['cases_95_credible'] = cases_95_credible
+    df['cases_05_credible'] = cases_05_credible
+
+    return df
+
+
+def plot_cases_for_addr(addr, color='r', alpha=.5, savePath=None, show=False):
+    try:
+        ((lat, lon), (county, state)) = cvd.get_location_of_address(addr, cvd.df_counties)
+        df_county = cvd.df_counties[(cvd.df_counties['county']==county) & ((cvd.df_counties['state']==state))]
+        dates = list(df_county['date'])
+        dates = [dateutil.parser.parse(d) for d in dates]
+        dates = [d.strftime("%B %-d") for d in dates]
+        cases = list(df_county['cases'])
+
+        fig = plt.figure()
+        fig.suptitle('Cases in %s County, %s' % (county, state), fontsize=14)
+        plt.xlabel('')
+        plt.ylabel('')
+        plt.xticks([0, len(cases)/2, len(cases)-1], rotation='horizontal', fontsize=12)
+        plt.bar(x=dates, height=cases, color='r', alpha=alpha)
+
+        if savePath != None:
+            filename = savePath + '/' + "%s, %s.png" % (county, state)
+            plt.savefig(filename, dpi=300)
+
+        if not(show):
+            plt.close(fig)
+        if savePath != None:
+            return filename
+        else:
+            return None
+
+    except Exception as e:
+        print(e)
+
+
+
+def plot_predictions_for_addr(addr, days_back=21, days_ahead=3, color='r', opacity=.6,
+                              savePath=None, show=False):
+    try:
+        ((lat, lon), (county, state)) = cvd.get_location_of_address(addr, cvd.df_counties)
+        df_county = cvd.df_counties[(cvd.df_counties['county']==county) & ((cvd.df_counties['state']==state))]
+        dates = list(df_county['date'])
+        dates = [dateutil.parser.parse(d) for d in dates]
+        latest_date = max(dates)
+        dates = [d.strftime("%B %-d") for d in dates]
+        cases = list(df_county['cases'])
+
+        this_df = df[(df['county']==county) & ((df['state']==state))]
+        dates1 = [dates[d] for d in np.arange(max(0,len(dates)-days_back), len(dates))]
+        cases1 = [cases[d] for d in np.arange(max(0,len(dates)-days_back), len(dates))]
+
+        next_dates = [latest_date + datetime.timedelta(days=i) for i in range(1, days_ahead+1)]
+        dates2 = [d.strftime("%B %-d") for d in next_dates]
+        cases2 = [int((this_df['population']/100000)*(this_df['cases_per_100k'] + i*this_df['delta_bar']))
+                      for i in range(1,days_ahead+1)]
+        up = [float((this_df['population']/100000)*this_df['delta_95']) for i in range(1,days_ahead+1)]
+        dn = [float((this_df['population']/100000)*this_df['delta_05']) for i in range(1,days_ahead+1)]
+        std = np.array([dn, up])
+
+        fig, ax = plt.subplots(figsize=(7,5))
+        cases_plt = plt.bar(x=dates1, height=cases1, color='r', alpha=opacity, label='cases')
+        preds_plt = plt.bar(x=dates2, height=cases2, color='r', alpha=.3, label='predicted')
+        err_plt = plt.errorbar(x=dates2, y=cases2, yerr=std, fmt='none', elinewidth=.75, capsize=1)
+        fig.suptitle('%s County, %s' % (county, state), fontsize=18)
+        plt.xlabel('')
+        plt.ylabel('')
+        plt.xticks([0, (len(dates1)-1)/2, len(dates1)-1], fontsize=14)
+        plt.legend(loc='upper left', frameon=False)
+
+        if savePath != None:
+            filename = savePath + '/' + "%s, %s.png" % (county, state)
+            plt.savefig(filename, dpi=300)
+
+        if not(show):
+            plt.close(fig)
+
+        if savePath != None:
+            return filename
+        else:
+            return None
+
+    except Exception as e:
+        print(e)
+
+
+df_recent, recent_date = process_recent_data(days_back=7)
+df_delta = compute_deltas(df_recent, days_back=3)
+df = initialize_for_simulation(df_delta)
+psi = run_gibbs_sampler(df)
+df = make_predictions(df, psi)
